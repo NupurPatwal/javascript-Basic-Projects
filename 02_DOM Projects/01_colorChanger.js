ea2c7008edc05837b4698612451db9d01bee63d9
Nupur Patwal <nupurpatwal@outlook.com>
2024-11-10 22:11:40 +0530

    01 project completd

--- 02_DOM Projects/01_colorChanger.js
+++ 02_DOM Projects/01_colorChanger.js
@@ -1,17 +1,14 @@
-// select all the elements aka btns:
+const buttons = document.querySelectorAll(".button");
 
-const btns = document.querySelectorAll(".button");
-console.log(btns);
-
-// select the body:
 const body = document.querySelector("body");
+// add event listner:
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    console.log("button clicked");
+    console.log(e.target);
 
-btns.forEach((button) => {
-  button.addEventListener("click", (event) => {
-    console.log("button is clicked.");
-    console.log(event.target);
-    if ((event.target.id )) {
-      body.style.backgroundColor = event.target.id;
+    if (e.target.id) {
+      body.style.backgroundColor = e.target.id;
     }
   });
 });
