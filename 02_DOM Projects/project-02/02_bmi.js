13921c8eae104e457de930547c697e9650173e28
Nupur Patwal <nupurpatwal@outlook.com>
2024-11-11 01:39:51 +0530

    bmi started

---
+++ 02_DOM Projects/project-02/02_bmi.js
@@ -0,0 +1,8 @@
+const form = document.querySelector("form");
+
+form.addEventListener("click", (e) => {
+  // to prevent the occuring:
+  e.preventDefault();
+});
+
+// https://stackblitz.com/edit/dom-project-chaiaurcode?file=2-BMICalculator%2Fstyle.css
